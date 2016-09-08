2b3a53094446ec71b83a11f03657a32bec10b845
Rahul_Rampura <rahul.rampura@gmail.com>
2016-09-08 14:13:18 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,31 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.8'
+gem 'mongoid', '5.1.4'
+gem 'bson', '4.1.1'
+gem 'bootstrap-sass'
+gem 'omniauth-google-oauth2', '~> 0.4.1'
+gem 'devise'
+
+group :development, :test do
+  gem 'rspec-rails', '3.5.2'
+end
+
+group :test do
+  gem 'selenium-webdriver'
+  gem 'capybara'
+end
+
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeaveApp::Application.config.secret_key_base = '69d215ea847a90c3f957cec5c8bfe532b80aa409df8a4f3cc983ea8cd89de42819847514ae714f4c01385c77ac5a006615313ff691b8d4cb089772e41bbe5f57'
